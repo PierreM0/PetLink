099accbc325963b346a08fc46861766617d07105
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-19 16:28:17 +0200

    icons extended retirés

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,6 @@ android {
 }
 
 dependencies {
-    // nos implémentations en plus
-    implementation(libs.androidx.compose.material.icons.extended)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
