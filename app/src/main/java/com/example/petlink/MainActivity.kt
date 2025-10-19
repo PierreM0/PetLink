0a3e63096c68d1f362a61e2b3f81580107cf30d3
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-19 18:40:50 +0200

    app démarre dans le navigation component

--- app/src/main/java/com/example/petlink/MainActivity.kt
+++ app/src/main/java/com/example/petlink/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.petlink.navigation.PetLinkNavigationComponent
 import com.example.petlink.ui.theme.PetLinkTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PetLinkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PetLinkNavigationComponent()
             }
         }
     }
