3fdb4f7e41501e8576e5daf2ea70282bf9fdd0d1
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-21 11:53:20 +0200

    dropdown avec value en paramètre

--- app/src/main/java/com/example/petlink/components/Dropdown.kt
+++ app/src/main/java/com/example/petlink/components/Dropdown.kt
@@ -1,9 +1,15 @@
 package com.example.petlink.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,25 +21,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun Dropdown(onValueChange: (String) -> Unit,
+fun Dropdown(value: String, onValueChange: (String) -> Unit,
              items: List<String> = listOf("Toutes espèces", "Chats", "Chiens", "Lapins")) { // TODO remove default param
     var isExpanded by remember { mutableStateOf(false) }
-    var itemPosition by remember { mutableIntStateOf(0) }
 
     Box(
         modifier = Modifier.padding(16.dp)
     ) {
-        Text(items[itemPosition])
+        Row(
+            modifier = Modifier.clickable { isExpanded = !isExpanded }
+        ) {
+            Text(value)
+            Icon(imageVector = if (isExpanded) Icons.Outlined.KeyboardArrowUp else Icons.Outlined.KeyboardArrowDown,
+                contentDescription = if (isExpanded) Icons.Outlined.KeyboardArrowUp.toString() else Icons.Outlined.KeyboardArrowDown.toString())
+        }
         DropdownMenu(
             expanded = isExpanded,
             onDismissRequest = { isExpanded = false }
         ) {
-            items.forEachIndexed { index, item ->
+            items.forEach { item ->
                 DropdownMenuItem(
                     text = { Text(item) },
                     onClick = {
                         isExpanded = false
-                        itemPosition = index
+                        onValueChange(item)
                     }
                 )
             }
