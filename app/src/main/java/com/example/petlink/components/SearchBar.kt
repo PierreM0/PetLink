7ff6d447bb77779d92e240d5a77db388146d3516
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-21 09:56:15 +0200

    search bar style amélioré

--- app/src/main/java/com/example/petlink/components/SearchBar.kt
+++ app/src/main/java/com/example/petlink/components/SearchBar.kt
@@ -1,20 +1,40 @@
 package com.example.petlink.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.example.petlink.ui.theme.White
 
 @Composable
 fun SearchBar(value: TextFieldValue, onValueChange: (TextFieldValue) -> Unit,
-              placeholderText: String) {
+              placeholderText: String, isLocationSearch: Boolean) {
     TextField(
+        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(16.dp)),
         value = value,
         onValueChange = onValueChange,
         placeholder = { Text(placeholderText) },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = White,
+            unfocusedContainerColor = White
+        ),
+        leadingIcon = { if (isLocationSearch) Icon(imageVector = Icons.Outlined.LocationOn, contentDescription = "Localisation")},
         trailingIcon = { Icon(imageVector = Icons.Outlined.Search, contentDescription = "Rechercher")}
     )
 }
