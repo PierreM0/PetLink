255c337374f334d32d25c3ff1befa4231e764a8f
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-21 15:01:31 +0200

    ajout Animal.kt

---
+++ app/src/main/java/com/example/petlink/model/Animal.kt
@@ -0,0 +1,10 @@
+package com.example.petlink.model
+
+data class Animal (
+    val id: Int,
+    val imageUrl: String,
+    val description: String,
+    val name: String,
+    val location: String,
+    val refuge: String
+)
