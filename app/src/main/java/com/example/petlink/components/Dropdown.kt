df9df2d95d5d9be1d680e1257522694cdbbf5a54
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-21 14:10:01 +0200

    style Dropdown.kt

--- app/src/main/java/com/example/petlink/components/Dropdown.kt
+++ app/src/main/java/com/example/petlink/components/Dropdown.kt
@@ -1,12 +1,19 @@
 package com.example.petlink.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
@@ -18,22 +25,34 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.petlink.ui.theme.White
 
 @Composable
 fun Dropdown(value: String, onValueChange: (String) -> Unit,
              items: List<String> = listOf("Toutes espèces", "Chats", "Chiens", "Lapins")) { // TODO remove default param
     var isExpanded by remember { mutableStateOf(false) }
 
-    Box(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Row(
-            modifier = Modifier.clickable { isExpanded = !isExpanded }
+    Box {
+        Card(
+            modifier = Modifier.fillMaxWidth()
+                .clickable { isExpanded = !isExpanded },
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = White
+            )
         ) {
-            Text(value)
-            Icon(imageVector = if (isExpanded) Icons.Outlined.KeyboardArrowUp else Icons.Outlined.KeyboardArrowDown,
-                contentDescription = if (isExpanded) Icons.Outlined.KeyboardArrowUp.toString() else Icons.Outlined.KeyboardArrowDown.toString())
+            Row(
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(value)
+                Icon(imageVector = if (isExpanded) Icons.Outlined.KeyboardArrowUp else Icons.Outlined.KeyboardArrowDown,
+                    contentDescription = if (isExpanded) Icons.Outlined.KeyboardArrowUp.toString() else Icons.Outlined.KeyboardArrowDown.toString())
+            }
         }
         DropdownMenu(
             expanded = isExpanded,
@@ -50,4 +69,11 @@ fun Dropdown(value: String, onValueChange: (String) -> Unit,
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun DropdownPreview() {
+    var a by remember { mutableStateOf("test")}
+    Dropdown(a, { b -> a = b})
 }
