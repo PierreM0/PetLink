80f8c4ded2cab69275d7129e1d661d8a4d957f95
Alexandre Pecci <pecci2u@etu.univ-lorraine.fr>
2025-10-21 18:31:11 +0200

    Modif ArticleCard + BlogScreen

--- app/src/main/java/com/example/petlink/components/ArticleCard.kt
+++ app/src/main/java/com/example/petlink/components/ArticleCard.kt
@@ -4,27 +4,39 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.petlink.model.Article
 import coil.compose.AsyncImage
+import com.example.petlink.model.Article
+import com.example.petlink.ui.theme.White
 
 @Composable
 fun ArticleCard(article : Article) {
-    Column(
-        modifier = Modifier.fillMaxSize().height(40.dp)
+    Card(
+        modifier = Modifier
+            .fillMaxSize(),
+        colors = CardDefaults.cardColors(White),
+        shape = RoundedCornerShape(16.dp)
     ) {
         AsyncImage(
             model = article.imageUrl,
             contentDescription = "Image of article ${article.title}",
             modifier = Modifier
-                .height(20.dp)
+                .height(140.dp)
                 .fillMaxWidth()
         )
-        Text(article.title)
-        Text(article.content)
-
+        Column(
+            modifier = Modifier.padding(16.dp)
+                .height(50.dp)
+        ) {
+            Text(article.title)
+            Text(article.content)
+        }
     }
 }
--- app/src/main/java/com/example/petlink/screens/BlogScreen.kt
+++ app/src/main/java/com/example/petlink/screens/BlogScreen.kt
@@ -1,12 +1,58 @@
 package com.example.petlink.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import com.example.petlink.components.ArticleCard
+import com.example.petlink.components.SearchBar
 import com.example.petlink.model.Article
+import com.example.petlink.ui.theme.BackgroundGreen
 import java.util.Date
+import kotlin.random.Random
 
 @Composable
 fun BlogScreen() {
-    var art : Article = Article(1, Date(10),"Un superbe titre","Un superbe article","https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSGxwl4fhem6m2urbLPsROL7UHP2tjF4CowszALXe5IwDIMG7C2VdCqSyLCsNi4mrqWuKJgVE215SQCpX3TOTQSUgwhOnN_kBr2FB6omLvWww")
-    ArticleCard(art)
+    var titleSearchValue by remember { mutableStateOf(TextFieldValue("") )}
+    Column(
+        modifier = Modifier.fillMaxSize().background(BackgroundGreen).padding(24.dp)
+    ) {
+        SearchBar(
+            value = titleSearchValue,
+            onValueChange = { value -> titleSearchValue = value},
+            placeholderText = "Rechercher par titre",
+            isLocationSearch = false
+        )
+        // TODO faire avec ArticleState avec ktor
+        var articles = mutableListOf<Article>()
+        for(i in 1..5)
+            articles.add( Article(1, Date(10),"Un superbe titre","Un superbe article","https://placedog.net/"+Random.nextInt(200, 801)))
+        val filteredList: List<Article> = articles.filter {
+            it.title.lowercase().contains(titleSearchValue.text.lowercase())
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            // TODO faire que ça soit un bouton pour vers une vue détaillée
+            items(filteredList) { article ->
+                ArticleCard(article)
+            }
+        }
+    }
 }
