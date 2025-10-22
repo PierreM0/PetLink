1a8c4a5012bd7c84303357bb25a30ac5e64f0514
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-22 16:30:28 +0200

    add AnimalViewModel et getArticles dans l'API

--- app/src/main/java/com/example/petlink/api/PetLinkAPI.kt
+++ app/src/main/java/com/example/petlink/api/PetLinkAPI.kt
@@ -1,8 +1,11 @@
 package com.example.petlink.api
 
 import com.example.petlink.mapper.AnimalMapper
+import com.example.petlink.mapper.ArticleMapper
 import com.example.petlink.model.Animal
 import com.example.petlink.model.AnimalDTO
+import com.example.petlink.model.Article
+import com.example.petlink.model.ArticleDTO
 import com.example.petlink.network.KtorClient
 import io.ktor.http.HttpMethod
 
@@ -11,6 +14,10 @@ object PetLinkAPI {
     private const val ANIMAL_ENDPOINT = "animals"
     private val animalMapper = AnimalMapper()
 
+    private lateinit var articles: List<Article>
+    private const val ARTICLE_ENDPOINT = "articles"
+    private val articleMapper = ArticleMapper()
+
     suspend fun getAnimals(): List<Animal> {
         if (!this::animals.isInitialized) {
             val animalsData: List<AnimalDTO> = KtorClient.httpCall(HttpMethod.Get, ANIMAL_ENDPOINT)
@@ -18,5 +25,14 @@ object PetLinkAPI {
         }
 
         return animals
+    }
+
+    suspend fun getArticles(): List<Article> {
+        if (!this::articles.isInitialized) {
+            val articlesData: List<ArticleDTO> = KtorClient.httpCall(HttpMethod.Get, ANIMAL_ENDPOINT)
+            articles = articlesData.map { articleMapper.mapArticleDtoToArticle(it) }
+        }
+
+        return articles
     }
 }
---
+++ app/src/main/java/com/example/petlink/viewmodels/AnimalViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.petlink.viewmodels
+
+class AnimalViewModel {
+}
