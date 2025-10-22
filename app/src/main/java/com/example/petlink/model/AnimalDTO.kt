fbadef9eb271ff060d0720187bfbf097316066bd
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-22 15:18:36 +0200

    animal mapper + DTO

---
+++ app/src/main/java/com/example/petlink/model/AnimalDTO.kt
@@ -0,0 +1,16 @@
+package com.example.petlink.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AnimalDTO (
+    val id: Int,
+    val imageUrl: String,
+    val description: String,
+    val name: String,
+    val location: String,
+    val refuge: String,
+    val species: String,
+    val birthdate: String,
+    val refugeNumber: String
+)
