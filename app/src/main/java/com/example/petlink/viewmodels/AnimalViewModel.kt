3fbbc4d2bb0aff7818d3ab4ef7cfd34fd4e37f7d
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-22 17:45:14 +0200

    animal view model

--- app/src/main/java/com/example/petlink/viewmodels/AnimalViewModel.kt
+++ app/src/main/java/com/example/petlink/viewmodels/AnimalViewModel.kt
@@ -1,4 +1,53 @@
 package com.example.petlink.viewmodels
 
-class AnimalViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.petlink.api.PetLinkAPI
+import com.example.petlink.model.Animal
+import com.example.petlink.network.StateManager
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class AnimalViewModel : ViewModel() {
+    private val animalsMutableStateFlow = MutableStateFlow(AnimalState())
+
+    val stateFlow: StateFlow<AnimalState>
+        get() = animalsMutableStateFlow.asStateFlow()
+
+    private var AnimalState: AnimalState
+        get() = animalsMutableStateFlow.value
+        set(value) {
+            animalsMutableStateFlow.value = value
+        }
+
+    init {
+        viewModelScope.launch {
+            getAnimalList()
+        }
+    }
+
+    fun getAnimalList() {
+        AnimalState = AnimalState.copy(isLoading = true)
+        StateManager.launchCoroutine {
+            AnimalState = try {
+                AnimalState.copy(
+                    animals = PetLinkAPI.getAnimals(),
+                    isLoading = false
+                )
+            } catch (error: Exception) {
+                AnimalState.copy(
+                    error = error,
+                    isLoading = false
+                )
+            }
+        }
+    }
+
+    fun setSelectedAnimal(animal: Animal) {
+        AnimalState = AnimalState.copy(
+            selectedAnimal = animal
+        )
+    }
 }
