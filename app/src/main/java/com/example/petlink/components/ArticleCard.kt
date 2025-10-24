461d0ea1ab30872f9706c2f70aaf1405fee4bd11
Alexandre Pecci <pecci2u@etu.univ-lorraine.fr>
2025-10-24 17:17:12 +0200

    Modif ArticleCard (C'est plus mieux)

--- app/src/main/java/com/example/petlink/components/ArticleCard.kt
+++ app/src/main/java/com/example/petlink/components/ArticleCard.kt
@@ -2,19 +2,19 @@ package com.example.petlink.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,39 +24,34 @@ import com.example.petlink.ui.theme.White
 
 @Composable
 fun ArticleCard(article : Article) {
-    Card(
+    ElevatedCard(
         modifier = Modifier.fillMaxWidth().height(150.dp),
         colors = CardDefaults.cardColors(White),
-        shape = RoundedCornerShape(16.dp)
+        shape = RoundedCornerShape(32.dp)
     ) {
         Row {
             AsyncImage(
                 model = article.imageUrl,
                 contentDescription = "Image of article ${article.title}",
-                modifier = Modifier
-                    .clip(RoundedCornerShape(10.dp))
-                    .size(150.dp)
+                modifier = Modifier.size(150.dp),
+                contentScale = ContentScale.FillBounds
             )
             Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize()
+                modifier = Modifier.padding(8.dp)
             ) {
                 Text(
                     text = article.title,
-                    fontSize = 13.sp,
+                    fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
-                    maxLines = 1,
+                    maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                     )
                 Text(
                     text = article.content,
-                    fontSize = 10.sp,
-                    lineHeight = 16.sp,
-                    modifier = Modifier.fillMaxSize(),
-                    maxLines = 6,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Justify,
                     overflow = TextOverflow.Ellipsis
-                    )
+                )
             }
         }
     }
