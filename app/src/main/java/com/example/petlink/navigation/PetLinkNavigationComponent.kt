3340baacc573d5ea5b92eecdb39d74832635899b
Alexandre Pecci <pecci2u@etu.univ-lorraine.fr>
2025-10-25 16:49:55 +0200

    No more LaunchedEffect

--- app/src/main/java/com/example/petlink/navigation/PetLinkNavigationComponent.kt
+++ app/src/main/java/com/example/petlink/navigation/PetLinkNavigationComponent.kt
@@ -4,12 +4,20 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,9 +25,12 @@ import com.example.petlink.components.BottomNavigationBar
 import com.example.petlink.screens.AdoptionScreen
 import com.example.petlink.screens.BlogDetailsScreen
 import com.example.petlink.screens.BlogScreen
+import com.example.petlink.ui.theme.MainGreen
+import com.example.petlink.ui.theme.White
 import com.example.petlink.viewmodels.ArticleState
 import com.example.petlink.viewmodels.ArticleViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("RestrictedApi")
 @Composable
 fun PetLinkNavigationComponent(articleViewModel: ArticleViewModel) {
@@ -27,11 +38,7 @@ fun PetLinkNavigationComponent(articleViewModel: ArticleViewModel) {
 
     val articleState: ArticleState = articleViewModel.stateFlow.collectAsState().value
 
-    LaunchedEffect(articleState.selectedArticle) {
-        if (articleState.selectedArticle != null) {
-            navController.navigate(PetLinkScreens.BlogDetailsScreen.name)
-        }
-    }
+    var topBarName by remember { mutableStateOf("Accueil - PetLink") }
 
     /*
     LaunchedEffect(Unit) {
@@ -51,6 +58,21 @@ fun PetLinkNavigationComponent(articleViewModel: ArticleViewModel) {
     */
 
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = topBarName,
+                        fontWeight = FontWeight.Bold,
+                        color = White,
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MainGreen
+                )
+            )
+        },
         bottomBar = { BottomNavigationBar(onIconClick = { route ->
             navController.popBackStack(route, true)
             navController.navigate(route)
@@ -61,26 +83,29 @@ fun PetLinkNavigationComponent(articleViewModel: ArticleViewModel) {
         ) {
             NavHost(navController = navController, startDestination = PetLinkScreens.HomeScreen.route) {
                 composable(PetLinkScreens.HomeScreen.route) {
-                    Text(PetLinkScreens.HomeScreen.title)
+                    topBarName = PetLinkScreens.HomeScreen.title
                 }
 
                 composable(PetLinkScreens.AdoptionScreen.route) {
+                    topBarName = PetLinkScreens.AdoptionScreen.title
                     AdoptionScreen()
                 }
 
                 composable(PetLinkScreens.HealthScreen.route) {
-                    Text(PetLinkScreens.HealthScreen.title)
+                    topBarName = PetLinkScreens.HealthScreen.title
                 }
 
                 composable(PetLinkScreens.BlogScreen.route) {
+                    topBarName = PetLinkScreens.BlogScreen.title
                     BlogScreen(articleState,
                         onDetails = { article ->
                             articleViewModel.setSelectedArticle(article)
+                            navController.navigate(PetLinkScreens.BlogDetailsScreen.name)
                         })
                 }
 
                 composable(PetLinkScreens.VeterinaryScreen.route) {
-                    Text(PetLinkScreens.VeterinaryScreen.title)
+                    topBarName = PetLinkScreens.VeterinaryScreen.title
                 }
 
                 composable(PetLinkScreens.BlogDetailsScreen.name) {
