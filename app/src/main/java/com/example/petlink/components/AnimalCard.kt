565f72a316e01dc6ee2dbb2f847d57409af03ee8
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-10-27 14:13:45 +0100

    selection d'un animal

--- app/src/main/java/com/example/petlink/components/AnimalCard.kt
+++ app/src/main/java/com/example/petlink/components/AnimalCard.kt
@@ -1,7 +1,5 @@
 package com.example.petlink.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -16,20 +14,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.petlink.model.Animal
-import com.example.petlink.ui.theme.White
 
 @Composable
-fun AnimalCard(animal: Animal) {
+fun AnimalCard(animal: Animal, backgroundColor: Color) {
     ElevatedCard(
         modifier = Modifier
             .width(112.dp).height(136.dp),
         shape = RoundedCornerShape(16.dp),
         colors = CardDefaults.cardColors(
-            containerColor = White
+            containerColor = backgroundColor
         )
     ) {
         Column(
--- app/src/main/java/com/example/petlink/screens/HealthRecordScreen.kt
+++ app/src/main/java/com/example/petlink/screens/HealthRecordScreen.kt
@@ -5,23 +5,33 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.petlink.components.AnimalCard
+import com.example.petlink.components.RawButton
 import com.example.petlink.model.Animal
 import com.example.petlink.ui.theme.BackgroundGreen
+import com.example.petlink.ui.theme.SubGreen
+import com.example.petlink.ui.theme.White
+import com.example.petlink.viewmodels.HealthRecordState
 import com.example.petlink.viewmodels.HealthRecordViewModel
 import java.time.LocalDate
 
 @Composable
 fun HealthRecordScreen(viewModel: HealthRecordViewModel = viewModel()) {
+    val state: HealthRecordState = viewModel.stateFlow.collectAsState().value
+
     Column(
         modifier = Modifier
             .background(BackgroundGreen)
@@ -35,26 +45,30 @@ fun HealthRecordScreen(viewModel: HealthRecordViewModel = viewModel()) {
 
         Spacer(Modifier.height(8.dp))
 
-        // TODO remove test animal
-        val animal = Animal(name = "Finger",
-            birthDate = LocalDate.now(),
-            pictureUrl = "https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?w=400&h=400&fit=crop")
-
-        LazyRow(
-            modifier = Modifier.padding(top = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            item {
-                AnimalCard(animal)
-            }
-            item {
-                AnimalCard(animal)
-            }
-            item {
-                AnimalCard(animal)
-            }
-            item {
-                AnimalCard(animal)
+        val animals = state.animals
+        if (animals.isEmpty()) {
+            Text(
+                text = "Vous n'avez pas ajouté d'animal",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+        else {
+            LazyRow(
+                modifier = Modifier.padding(top = 8.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(animals) { animal ->
+                    val isSelected = state.selectedAnimal === animal
+                    RawButton(onClick = {
+                        viewModel.setSelectedAnimal(animal)
+                    }) {
+                        AnimalCard(
+                            animal = animal,
+                            backgroundColor = if (isSelected) SubGreen else White
+                        )
+                    }
+                }
             }
         }
     }
