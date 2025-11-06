8745a87096933b90d4f8e903c760891ee7b475f0
Alexandre Pecci <pecci2u@etu.univ-lorraine.fr>
2025-11-06 11:50:06 +0100

    Modif SearchBar

--- app/src/main/java/com/example/petlink/screens/AdoptionScreen.kt
+++ app/src/main/java/com/example/petlink/screens/AdoptionScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -61,7 +63,9 @@ fun AdoptionScreen(adoptionAnimalViewModel: AdoptionAnimalViewModel, onDetails: 
                     adoptionAnimalViewModel.setLocationFilter(value.text)
                                 },
                 placeholderText = "Rechercher par localisation",
-                isLocationSearch = true)
+                isLocationSearch = true,
+                modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(16.dp))
+            )
 
             // Filtres espèces & âges
             Row(
