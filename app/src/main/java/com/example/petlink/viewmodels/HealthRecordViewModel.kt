15b7f79b7559cf47decb4e353c5009a3ab12d958
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-11-06 12:53:51 +0100

    HealthRecordViewModel : opti imports

--- app/src/main/java/com/example/petlink/viewmodels/HealthRecordViewModel.kt
+++ app/src/main/java/com/example/petlink/viewmodels/HealthRecordViewModel.kt
@@ -1,16 +1,11 @@
 package com.example.petlink.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.petlink.model.Animal
 import com.example.petlink.model.AnimalEvent
-import com.example.petlink.model.AnimalEventType
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import java.time.LocalDate
 
 class HealthRecordViewModel : ViewModel() {
     private val healthRecordMutableStateFlow = MutableStateFlow(HealthRecordState())
