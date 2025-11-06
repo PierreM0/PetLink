14df2ff1866fdaf08b4e7efe9b14ce7f5c8645a5
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-11-06 11:14:46 +0100

    AddAnimalFormScreen : opti imports

--- app/src/main/java/com/example/petlink/screens/AddAnimalFormScreen.kt
+++ app/src/main/java/com/example/petlink/screens/AddAnimalFormScreen.kt
@@ -2,25 +2,14 @@ package com.example.petlink.screens
 
 import android.net.Uri
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DateRangePicker
-import androidx.compose.material3.DateRangePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDatePickerState
-import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -29,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -39,13 +27,10 @@ import com.example.petlink.components.FormButtons
 import com.example.petlink.components.InputDateField
 import com.example.petlink.components.InputImageField
 import com.example.petlink.components.InputTextField
-import com.example.petlink.components.RawButton
 import com.example.petlink.model.Animal
 import com.example.petlink.model.AnimalSpecies
 import com.example.petlink.ui.theme.BackgroundGreen
-import com.example.petlink.ui.theme.FormPreviousGray
 import com.example.petlink.ui.theme.MainGreen
-import com.example.petlink.ui.theme.White
 import java.time.LocalDate
 
 @OptIn(ExperimentalMaterial3Api::class)
